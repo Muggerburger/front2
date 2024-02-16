c320d8ca7c6bcf93c1e159e1a9fb4071b6ac4b0d
steamedEggMaster <cksdud5014@naver.com>
2024-02-17 04:55:39 +0900

    로그인 성공

--- src/comp/login/LoginHandler.jsx
+++ src/comp/login/LoginHandler.jsx
@@ -3,17 +3,17 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const LoginHandeler = (props) => {
-  const navigate = useNavigate();
+  /*const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
 
     const kakaoLogin = async () => {
-      await axios.get(`/kakao/login/?code=${code}`)
+      await axios.get(`http://localhost:8080/login?code=${code}`) // 1. url을 8080으로
       .then(res => {
         console.log(res);
         //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
         localStorage.setItem("name", res.data.account.kakaoName);
         //로그인이 성공하면 이동할 페이지
-        navigate("/inhome");
+        navigate(`/home`);
     })
     .catch(error => {
         console.error('오류 발생:', error.response);
@@ -23,7 +23,25 @@ const LoginHandeler = (props) => {
  
   useEffect(() => {
     kakaoLogin();
-  }, [props.history]);
+  }, [props.history]); */
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get("code");
+
+  const kakaoLogin = async () => {
+    try {
+      const res = await axios.get(`http://localhost:8080/login?code=${code}`);
+      console.log(res);
+      // 로그인이 성공하면 이동할 페이지로 navigate
+      navigate(`/home?code=${code}`);
+    } catch (error) {
+      console.error('오류 발생:', error.response);
+      alert('실패했습니다.');
+    }
+  };
+
+  useEffect(() => {
+    kakaoLogin();
+  }, []);
 
 
   return (
